abdb4b85a5b4f8e0480060b41fe216b3f952a1df
David Wilkinson <david@gslmedia.co.uk>
2016-02-29 14:56:41 +0000

    Edited custom attribute note message

--- app/code/local/Samuidavid/AttributeHelpers/sql/attributehelpers/mysql4-upgrade-0.0.1-0.0.2.php
+++ app/code/local/Samuidavid/AttributeHelpers/sql/attributehelpers/mysql4-upgrade-0.0.1-0.0.2.php
@@ -1,3 +1,3 @@
 <?php
 
-$this->updateAttribute('catalog_product', 'sw_features_title', 'note', 'This will be prepended to the Scissor Features title on product view pages (e.g. - "Senzei Zip Scissor Features"). Leave this blank for product\'s Name to appear...');
+$this->updateAttribute('catalog_product', 'custom_att_code', 'note', 'This comment will be seen as a comment in the admin area of Magento underneath an attribute field...');
