f1b221024a583212c5476c74c82e108a700368fa
David Wilkinson <david@gslmedia.co.uk>
2016-02-29 14:45:45 +0000

    Initial commit

---
+++ app/code/local/Samuidavid/AttributeHelpers/sql/attributehelpers/mysql4-install-0.0.1.php
@@ -0,0 +1,3 @@
+<?php
+
+$this->updateAttribute('catalog_product', 'custom_att_code', 'note', 'This comment will be seen as a comment in the admin area of Magento underneath an attribute field...');
---
+++ app/code/local/Samuidavid/AttributeHelpers/sql/attributehelpers/mysql4-upgrade-0.0.1-0.0.2.php
@@ -0,0 +1,3 @@
+<?php
+
+$this->updateAttribute('catalog_product', 'sw_features_title', 'note', 'This will be prepended to the Scissor Features title on product view pages (e.g. - "Senzei Zip Scissor Features"). Leave this blank for product\'s Name to appear...');
